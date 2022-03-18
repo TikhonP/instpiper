6b6dc580649b4529c4fd411542ea2ab7c314ad2c
EraChanZ <39560922+EraChanZ@users.noreply.github.com>
2022-03-18 13:38:18 +0100

    Update testtest.py

--- analysis/testtest.py
+++ analysis/testtest.py
@@ -1,3 +1,3 @@
-from ANOHC import HitlerClassifier
-hc = HitlerClassifier("task_files/0ba7377f602182d1a5c4dac25f5d10955fe6c012_proxy.txt", "task_files/0ba7377f602182d1a5c4dac25f5d10955fe6c012.txt", 4, {"is_parsed":False, 'is_id':False, 'from_id':0})
+from ANOHC import MainClassifier
+hc = MainClassifier("task_files/0ba7377f602182d1a5c4dac25f5d10955fe6c012_proxy.txt", "task_files/0ba7377f602182d1a5c4dac25f5d10955fe6c012.txt", 4, {"is_parsed":False, 'is_id':False, 'from_id':0})
 hc.start()
